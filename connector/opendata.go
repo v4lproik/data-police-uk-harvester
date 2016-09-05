be3ecc5abf0f29827857e63fa3884a213ca1a1ee
v4lproik <v4lproik@gmail.com>
2016-09-05 15:04:47 +0200

    [data] Unmarshall every fields from json response

--- connector/opendata.go
+++ connector/opendata.go
@@ -13,10 +13,34 @@ const API = "/stops-street"
 
 type Stop struct {
 	AgeRange string `json:"age_range"`
+	SelfDefinedEthnicity string `json:"self_defined_ethnicity"`
+	DateTime string `json:"datetime"`
+	RemovalOfMoreThanOuterClothing bool `json:"removal_of_more_than_outer_clothing"`
+	Operation string `json:"operation"`
+	OfficerDefinedEthnicity string `json:"officer_defined_ethnicity"`
+	ObjectOfSearch string `json:"object_of_search"`
+	InvolvedPerson bool `json:"involved_person"`
+	Gender string `json:"gender"`
+	Legislation string `json:"legislation"`
+	Outcome json.RawMessage `json:"outcome"`
+	Type string `json:"type"`
+	OperationName string `json:"operation_name"`
+	Location Location `json:"location"`
 }
 
-func GetData(lat float64, lng float64, year int64, month int64) ([]Stop, error) {
+type Location struct {
+	Latitude string `json:"latitude"`
+	Longitude string `json:"longitude"`
+	Street Street `json:"street"`
+}
 
+type Street struct {
+	Id int64 `json:"id"`
+	Name string `json:"name"`
+}
+
+
+func GetData(lat float64, lng float64, year int64, month int64) ([]Stop, error) {
 
 	url := DOMAIN + API + "?lat=" + strconv.FormatFloat(lat, 'f', -1, 64) + "&lng=" + strconv.FormatFloat(lng, 'f', -1, 64) + "&date=" + strconv.FormatInt(year, 10) + "-" + strconv.FormatInt(month, 10)
 
@@ -36,7 +60,7 @@ func GetData(lat float64, lng float64, year int64, month int64) ([]Stop, error) 
 }
 
 func getStops(body []byte) ([]Stop, error) {
-	items := make([]Stop, 100)
+	items := make([]Stop, 10)
 
 	err := json.Unmarshal(body, &items)
 	if(err != nil){
